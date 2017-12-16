955a9d3a52fe27943dbfd3b95e3b8d6180832788
Shantanu Odak <so2362njit.edu>
2017-12-16 01:29:13 -0500

    Todo_edit created to edit the todo items

---
+++ pages/todo_edit.php
@@ -0,0 +1,33 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>The HTML5 Herald</title>
+    <meta name="description" content="The HTML5 Herald">
+    <meta name="author" content="SitePoint">
+
+    <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+    <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+
+<form action="index.php?page=tasks&action=save&id=<?php echo $data->id;?>" method="post">
+    Owner Email:<input type="text" name="owneremail" value= "<?php echo $data->owneremail;?> "> <br>
+    Due date: <input type="text" name="duedate" value="<?php echo $data->duedate;?> " > <br>
+    Message: <input type="text" name="message" value="<?php echo $data->message;?> "> <br>
+    Is Done: <input type="text" name="isdone" value="<?php echo $data->isdone;?>  "> <br>
+
+    <input type="submit" value="Submit form">
+</form>
+
+
+<script src="js/scripts.js"></script>
+</body>
+</html>
