2beba39f7de0f9c439580f3866820677ff9fd6d9
Shantanu Odak <so2362njit.edu>
2017-12-16 07:25:11 -0500

    Added header and footer for the home page.

--- pages/homepage.php
+++ pages/homepage.php
@@ -1,21 +1,6 @@
-<!doctype html>
 
-<html lang="en">
-<head>
-    <meta charset="utf-8">
 
-    <title>The HTML5 Herald</title>
-    <meta name="description" content="The HTML5 Herald">
-    <meta name="author" content="SitePoint">
-
-    <link rel="stylesheet" href="css/styles.css?v=1.0">
-
-    <!--[if lt IE 9]>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
-    <![endif]-->
-</head>
-
-<body>
+<?php include 'header.php' ?>
 
 
 <h1>
@@ -26,29 +11,22 @@
 
     ?> </h1>
 
-<h1><a href="index.php?page=accounts&action=all">Show All Accounts</a></h1>
-<h1><a href="index.php?page=tasks&action=all">Show All Tasks</a></h1>
 
-<h1><a href="index.php?page=accounts&action=Logout">Logout</a></h1>
+<?php
+ session_start();
 
+ if(key_exists( 'userID', $_SESSION)) {
 
-<form action="index.php?page=accounts&action=login" method="POST">
+     echo '<h1><a href="index.php?page=tasks&action=all">Show All Tasks</a></h1>';
+     echo '<h1><a href="index.php?page=accounts&action=Logout">Logout</a></h1>';
+     echo '<h1><a href="index.php?page=accounts&action=all">Show All Accounts</a></h1>';
 
-    <div class="container">
-        <label><b>Username</b></label>
-        <input type="text" placeholder="Enter Username" name="email" required>
+ } else {
 
-        <label><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="password" required>
+     include 'login_form.php';
 
-        <button type="submit">Login</button>
-    </div>
-
-
-</form>
-<h1><a href="index.php?page=accounts&action=register">Register</a></h1>
+ }
+ ?>
 
 
-<script src="js/scripts.js"></script>
-</body>
-</html>
+<?php include 'footer.php' ?>
---
+++ pages/login_form.php
@@ -0,0 +1,17 @@
+
+<form action="index.php?page=accounts&action=login" method="POST">
+
+    <div class="container">
+        <label><b>Username</b></label>
+        <input type="text" placeholder="Enter Username" name="email" required>
+
+        <label><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="password" required>
+
+        <button type="submit">Login</button>
+    </div>
+
+
+</form>
+
+<h1><a href="index.php?page=accounts&action=register">Register</a></h1>
